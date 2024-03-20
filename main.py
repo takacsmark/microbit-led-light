7f07a4b754c58ef0e5778072103c5bfcaf68912d
Mark Takacs <takacs.mark@icloud.com>
2024-03-20 14:47:55 +0100

    5 patterns

--- main.py
+++ main.py
@@ -31,14 +31,43 @@ def backward():
 
 def back_and_forth():
     for i in range(REPEAT):
-        forward()
+        for pin in range(PINS.length):
+                        flash_led(pin)
         for pin in range(PINS.length -2, 0, -1): 
             flash_led(pin)
+
+def fill_flash():
+    for i in range(REPEAT):
+        for pin in range(PINS.length):
+            pins.digital_write_pin(PINS[pin], 1)
+            control.wait_micros(LIGHT_TIME)
+
+        for j in range(REPEAT):
+            for pin in range(PINS.length):
+                pins.digital_write_pin(PINS[pin], 0)
+            
+            control.wait_micros(LIGHT_TIME)
+
+            for pin in range(PINS.length):
+                pins.digital_write_pin(PINS[pin], 1)
+
+            control.wait_micros(LIGHT_TIME)
+
+        for pin in range(PINS.length):
+            pins.digital_write_pin(PINS[pin], 0)
+
+def half_flash():
+    for i in range(REPEAT * REPEAT):
+        for pin in range(PINS.length):
+            pins.digital_write_pin(PINS[pin], i % 2) if pin < PINS.length /2 else pins.digital_write_pin(PINS[pin], 1 - i % 2)
+        control.wait_micros(LIGHT_TIME)
 
 led.enable(False)
 
 def on_forever():
-    forward()
-    backward()  
+    forward() 
+    backward() 
     back_and_forth()
+    fill_flash()
+    half_flash()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -24,11 +24,46 @@ function backward() {
 }
 
 function back_and_forth() {
+    let pin: number;
     for (let i = 0; i < REPEAT; i++) {
-        forward()
-        for (let pin = PINS.length - 2; pin > 0; pin += -1) {
+        for (pin = 0; pin < PINS.length; pin++) {
             flash_led(pin)
         }
+        for (pin = PINS.length - 2; pin > 0; pin += -1) {
+            flash_led(pin)
+        }
+    }
+}
+
+function fill_flash() {
+    let pin: number;
+    for (let i = 0; i < REPEAT; i++) {
+        for (pin = 0; pin < PINS.length; pin++) {
+            pins.digitalWritePin(PINS[pin], 1)
+            control.waitMicros(LIGHT_TIME)
+        }
+        for (let j = 0; j < REPEAT; j++) {
+            for (pin = 0; pin < PINS.length; pin++) {
+                pins.digitalWritePin(PINS[pin], 0)
+            }
+            control.waitMicros(LIGHT_TIME)
+            for (pin = 0; pin < PINS.length; pin++) {
+                pins.digitalWritePin(PINS[pin], 1)
+            }
+            control.waitMicros(LIGHT_TIME)
+        }
+        for (pin = 0; pin < PINS.length; pin++) {
+            pins.digitalWritePin(PINS[pin], 0)
+        }
+    }
+}
+
+function half_flash() {
+    for (let i = 0; i < REPEAT * REPEAT; i++) {
+        for (let pin = 0; pin < PINS.length; pin++) {
+            pin < PINS.length / 2 ? pins.digitalWritePin(PINS[pin], i % 2) : pins.digitalWritePin(PINS[pin], 1 - i % 2)
+        }
+        control.waitMicros(LIGHT_TIME)
     }
 }
 
@@ -37,4 +72,6 @@ basic.forever(function on_forever() {
     forward()
     backward()
     back_and_forth()
+    fill_flash()
+    half_flash()
 })
