fa68ce68a7eae2ba0f624733cffe74f6a65335f9
Mark Takacs <takacs.mark@icloud.com>
2024-03-20 13:20:01 +0100

    add pins

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 LIGHT_TIME = 250000
+REPEAT = 2
 
 PINS = [
     DigitalPin.P0,
     DigitalPin.P1,
     DigitalPin.P2,
     DigitalPin.P3,
-    DigitalPin.P13,
-    DigitalPin.P14,
-    DigitalPin.P15,
-    DigitalPin.P16,
+    DigitalPin.P4,
+    DigitalPin.P5,
+    DigitalPin.P6,
+    DigitalPin.P7,
+    DigitalPin.P8,
+    DigitalPin.P9,
 ]
 
+def repeat(times, action):
+    for i in range(times):
+        action()
+
 def forward():
-    for pin in PINS:
+    for pin in PINS.reverse():
         pins.digital_write_pin(pin, 1)
         control.wait_micros(LIGHT_TIME)
         pins.digital_write_pin(pin, 0)
 
+def backward():
+    for pin in PINS.reverse():
+        pins.digital_write_pin(pin, 1)
+        control.wait_micros(LIGHT_TIME)
+        pins.digital_write_pin(pin, 0)
+
+
 def on_forever():
     led.enable(False)
-    forward()
+    for i in range(2):
+        forward()
+    
+    control.wait_micros(1000000)
+
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
 let LIGHT_TIME = 250000
-let PINS = [DigitalPin.P0, DigitalPin.P1, DigitalPin.P2, DigitalPin.P3, DigitalPin.P13, DigitalPin.P14, DigitalPin.P15, DigitalPin.P16]
+let REPEAT = 4
+let PINS = [DigitalPin.P0, DigitalPin.P1, DigitalPin.P2, DigitalPin.P3, DigitalPin.P4, DigitalPin.P5, DigitalPin.P6, DigitalPin.P7, DigitalPin.P8, DigitalPin.P9]
+function repeat(times: any, action: any) {
+    for (let i = 0; i < times; i++) {
+        action
+    }
+}
+
 function forward() {
     for (let pin of PINS) {
         pins.digitalWritePin(pin, 1)
